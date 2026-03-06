6e3c2ea1449a931515ca18a7ba23fec38cb6ece6
borisdrab <borkodrab@gmail.com>
2026-03-06 16:49:50 +0100

    PR review fixes: removed Genre relation and cleaned gitignore confl

--- Launcher.DAL/Entities/GameTitleEntity.cs
+++ Launcher.DAL/Entities/GameTitleEntity.cs
@@ -17,9 +17,6 @@ public class GameTitleEntity : IEntity
     public DateTime? ReleaseDate  { get; set; }
     public bool IsAvailable { get; set; } = true;
     
-   // public Guid? GenreId { get; set; }
-    //public GenreEntity? Genre { get; set; }
-    
     public ICollection<LibraryTitleEntity> LibraryTitles { get; set; } = new List<LibraryTitleEntity>();
     public ICollection<ReviewEntity> Reviews { get; set; } = new List<ReviewEntity>();
     public ICollection<GameTitlePlatformEntity> GameTitlePlatforms { get; set; } = new List<GameTitlePlatformEntity>();
