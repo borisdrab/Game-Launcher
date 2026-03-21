a21788311ad57cf4561544366c0fe946184b932d
borisdrab <borkodrab@gmail.com>
2026-03-21 15:56:33 +0100

    #2 Fix after review: Remove Hello World from Program.cs

--- Game-Launcher/Program.cs
+++ Game-Launcher/Program.cs
@@ -1,3 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿namespace Game_Launcher;
 
-Console.WriteLine("Hello, World!");
+public class Program
+{
+    public static void Main(string[] args)
+    {
+    }
+}
