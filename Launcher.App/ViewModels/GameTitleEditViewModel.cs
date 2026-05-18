cfa718b5032bfdf7d506bb5da07218e47d3945f3
OndrejRohar <vovooo.rohar@gmail.com>
2026-05-18 23:45:33 +0200

    Added empty gametitle edit page and viewmodel

---
+++ Launcher.App/ViewModels/GameTitleEditViewModel.cs
@@ -0,0 +1,18 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Launcher.App.Messages;
+using Launcher.App.Services;
+using Launcher.BL.Facades.Interfaces;
+using Launcher.BL.Models;
+
+namespace Launcher.App.ViewModels;
+
+public partial class GameTitleEditViewModel(
+    IGameTitleFacade gameTitleFacade,
+    INavigationService navigationService,
+    IMessengerService messengerService,
+    IAlertService alertService)
+    : ViewModelBase(messengerService)
+{
+    public Guid Id { get; set; }
+}
